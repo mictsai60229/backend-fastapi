015b824291f271efc192cc471104cfc90847776d
mictsai60229 <mictsai@nlplab.cc>
2021-02-17 11:55:27 +0800

    style: exc -> exception

--- backend/exceptions/exception_handlers.py
+++ backend/exceptions/exception_handlers.py
@@ -8,24 +8,24 @@ from starlette.status import HTTP_400_BAD_REQUEST
 from config.response import STATUS
 
 
-async def http_exception_handler(request: Request, exc: HTTPException) -> JSONResponse:
-    headers = getattr(exc, "headers", None)
-    metadata = getattr(exc, "metadata", None)
+async def http_exception_handler(request: Request, excpetion: HTTPException) -> JSONResponse:
+    headers = getattr(excpetion, "headers", None)
+    metadata = getattr(excpetion, "metadata", None)
     metadata = metadata or {'status': '9999', 'desc': STATUS['9999']}
-    data = getattr(exc, "data", None)
+    data = getattr(excpetion, "data", None)
     
     if headers:
         return JSONResponse(
-            {"metadata": metadata, "data": data}, status_code=exc.status_code, headers=headers
+            {"metadata": metadata, "data": data}, status_code=excpetion.status_code, headers=headers
         )
     else:
-        return JSONResponse({"metadata": metadata, "data": data}, status_code=exc.status_code)
+        return JSONResponse({"metadata": metadata, "data": data}, status_code=excpetion.status_code)
 
 
 async def request_validation_exception_handler(
-    request: Request, exc: RequestValidationError
+    request: Request, excpection: RequestValidationError
 ) -> JSONResponse:
-    metadata = {"status": "1000", "desc":jsonable_encoder(exc.errors())}
+    metadata = {"status": "1000", "desc":jsonable_encoder(excpection.errors())}
     return JSONResponse(
         status_code=HTTP_400_BAD_REQUEST,
         content={"metadata": metadata, "data": None, },
